995aba1e184cdc9ee8f4435c763200e2af0b2cc5
naviantar-ptdh <navi.antar@ptdh.co.id>
2026-07-09 15:20:02 +0700

    Fix image filename in app.py

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ if "units_df" not in st.session_state:
 def main():
    col1, col2 = st.columns([1, 8], vertical_alignment="center")
    with col1:
-        st.image("logo_putih.png", width=70)
+        st.image("logo_putih", width=70)
    with col2:
         st.title("FTE Calculator")
    try:
